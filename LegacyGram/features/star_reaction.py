43f46fa1aa67cdf7af6415fbc95d771e7ebc1e5d
repinek <137826826+repinek@users.noreply.github.com>
2026-03-09 22:10:25 +0500

    fix: remove gold gradient when Hide star reaction

--- LegacyGram/features/star_reaction.py
+++ LegacyGram/features/star_reaction.py
@@ -1,4 +1,4 @@
-from hook_utils import find_class
+from hook_utils import find_class, set_private_field
 
 from LegacyGram.data.constants import Keys
 from LegacyGram.utils.xposed_utils import BaseHook
@@ -49,6 +49,17 @@ class ReactionsContainerLayoutSetVisibleReactionsListHook(BaseHook):
             visible_reactions_list.remove(index_to_remove)
 
 
+class ReactionsContainerLayoutDrawHook(BaseHook):
+    """Remove gold gradient"""
+
+    def before_hooked_method(self, param):
+        if not self.is_enabled():
+            return
+
+        instance = param.thisObject
+        set_private_field(instance, "hasStar", False)
+
+
 def register_star_reaction(plugin) -> None:
     ReactionsLayoutInBubble = find_class("org.telegram.ui.Components.Reactions.ReactionsLayoutInBubble")
     if ReactionsLayoutInBubble:
@@ -59,3 +70,4 @@ def register_star_reaction(plugin) -> None:
         plugin.hook_all_methods(
             ReactionsContainerLayout, "setVisibleReactionsList", ReactionsContainerLayoutSetVisibleReactionsListHook(plugin, Keys.hide_star_reaction)
         )
+        plugin.hook_all_methods(ReactionsContainerLayout, "dispatchDraw", ReactionsContainerLayoutDrawHook(plugin, Keys.hide_star_reaction))
